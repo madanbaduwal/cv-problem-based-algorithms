c36bf9f74c6b6a6b00b1cb9a16a14ae2160cc5b1
MadanBaduwal <MadanBaduwal@users.noreply.github.com>
2022-08-14 20:22:08 +0000

    deploy: a6d1cddf45fae676d8fe907b873230205ff09d75

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/MadanBaduwal-problem-based-computer-vision-algorithms/assets/js/search-data.json', true);
+    request.open('GET', '/problem-based-computer-vision-algorithms/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
